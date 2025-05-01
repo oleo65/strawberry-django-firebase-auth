05318b98be9b081441b772ddf30df24806ad2692
Ole Odendahl <oleodendahl@gmail.com>
2025-05-01 21:04:17 +0200

    Fixed async compatibility for authentication flow.

--- strawberry_django_firebase_auth/authentication.py
+++ strawberry_django_firebase_auth/authentication.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Any
 
+from asgiref.sync import sync_to_async
 from django.contrib.auth import get_user_model
 from firebase_admin import auth
 
@@ -14,20 +15,23 @@ User = get_user_model()
 class FirebaseAuthentication:
 
     def _get_auth_token(self, request) -> dict[str, Any] | None:
-        encoded_token = request.META.get(firebase_auth_settings.AUTH_HEADER_NAME)
+        encoded_token = request.META.get(
+            firebase_auth_settings.AUTH_HEADER_NAME)
         decoded_token = None
 
         try:
             decoded_token = auth.verify_id_token(encoded_token, firebase_app)
         except ValueError:
             pass
-        except (auth.InvalidIdTokenError, auth.ExpiredIdTokenError, auth.RevokedIdTokenError) as err:
+        except (auth.InvalidIdTokenError, auth.ExpiredIdTokenError,
+                auth.RevokedIdTokenError) as err:
             logging.error(err.default_message)
         except auth.CertificateFetchError as err:
             logging.exception(err)
         return decoded_token
 
-    def _get_user_from_firebase_user(self, firebase_user: dict[str, Any]):
+    async def _get_user_from_firebase_user(self, firebase_user: dict[str,
+                                                                     Any]):
         firebase_uid = firebase_user.get('uid')
         user = None
 
@@ -37,55 +41,61 @@ class FirebaseAuthentication:
             else:
                 expression = firebase_auth_settings.FIREBASE_UID_PROPERTY
 
-            user = User.objects.get(**{expression: firebase_uid})
+            user = await User.objects.aget(**{expression: firebase_uid})
         except User.DoesNotExist:
-            user = self._register_unregistered_user(firebase_user)
+            user = await self._register_unregistered_user(firebase_user)
         return user
 
-    def _register_unregistered_user(self, firebase_user: dict[str, Any]):
+    async def _register_unregistered_user(self, firebase_user: dict[str, Any]):
         user = None
 
-        user = self._match_user_by_email(firebase_user)
+        user = await self._match_user_by_email(firebase_user)
 
         if user is None:
-            user = User.objects.create_user(
+            user = await User.objects.acreate_user(
                 username=firebase_user['uid'],
                 email=firebase_user['email'],
             )
 
-            user = self._set_firebase_uid(user, firebase_user)
+            user = await self._set_firebase_uid(user, firebase_user)
 
         return user
 
-    def _match_user_by_email(self, firebase_user):
+    async def _match_user_by_email(self, firebase_user):
         user = None
 
         try:
-            user = User.objects.get(email=firebase_user['email'])
+            user = await User.objects.aget(email=firebase_user['email'])
 
-            user = self._set_firebase_uid(user, firebase_user)
+            user = await self._set_firebase_uid(user, firebase_user)
         except User.DoesNotExist:
             pass
 
         return user
 
-    def _set_firebase_uid(self, user, firebase_user):
+    async def _set_firebase_uid(self, user, firebase_user):
         if firebase_auth_settings.PROFILE_MODEL:
-            profile = getattr(user, firebase_auth_settings.PROFILE_MODEL)
-            setattr(profile, firebase_auth_settings.FIREBASE_UID_PROPERTY, firebase_user['uid'])
-            profile.save()
+            profile = await sync_to_async(getattr)(user, firebase_auth_settings.PROFILE_MODEL)
+            setattr(profile, firebase_auth_settings.FIREBASE_UID_PROPERTY,
+                    firebase_user['uid'])
+            await profile.asave()
         else:
             user.firebase_uid = firebase_user['uid']
-            user.save()
+            await user.asave()
 
         return user
 
     def authenticate(self, request):
+        raise NotImplementedError(
+            "Synchronous API is not available for auth anymore. Use async API instead."
+        )
+
+    async def aauthenticate(self, request):
         user = None
         firebase_user = self._get_auth_token(request)
 
         if firebase_user:
-            user = self._get_user_from_firebase_user(firebase_user)
+            user = await self._get_user_from_firebase_user(firebase_user)
         return user
 
     def get_user(self, user_pk):
